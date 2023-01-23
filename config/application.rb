f362f0743b3c1ebe2fa44d92b375fed44b714a95
martin-creator <martinlubowa@outlook.com>
2023-01-23 21:35:05 +0300

    Updated files

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,7 @@ module Africalancer
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
     #config.action_dispatch.cookies_same_site_protection = :none
+    config.assets.initialize_on_precompile = false
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
